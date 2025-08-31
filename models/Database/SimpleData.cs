a23e4762f4041706407addb97cb5585aa3e62a6f
Panos Anagnostou <panosanag90@gmail.com>
2025-08-31 03:30:29 +0300

    add database model

---
+++ models/Database/SimpleData.cs
@@ -0,0 +1,31 @@
+using Supabase;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace simple_backend_dotnet.models.Database
+{
+    [Table("simple-data")]
+    public class SimpleData : BaseModel
+    {
+        [PrimaryKey("id")]
+        public int Id { get; set; }
+
+        [Column("created")]
+        public DateTime Created { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("price")]
+        public float Price { get; set; }
+
+        [Column("link")]
+        public string Link { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("currency")]
+        public string Currency { get; set; }
+    }
+}
